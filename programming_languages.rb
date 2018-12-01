b2b1f7db94047665dd351ecffb6e2d21bea8388d
Ethan Rosenberg <ethan@ethanrosenberg.com>
2018-12-01 09:50:28 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,17 @@
 def reformat_languages(languages)
   # your code here
+  
+  myhash = {}
+  
+  languages.each do |style, value|
+  
+    value.each do |language, att|
+      myhash[language] = {
+        :type => att,
+        :style => [style]
+      }
+    end
+  myhash
+  end
+  
 end
